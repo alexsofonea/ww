27a350437e55fb8b7ad7b8fd239c306926ebe982
Alex Sofonea <alex@alexsofonea.com>
2024-10-09 23:44:28 +0300

    Certificate works!

--- server/certificate/test.php
+++ server/certificate/test.php
@@ -13,30 +13,27 @@ $ac->loadAccountKey(file_get_contents('cert/account_key.pem'));
 $ret=$ac->register(true,'certificate@ww.alexsofonea.com');
 print_r($ret);*/
 
+//   /var/services/web/ww/server/secureHost/acme-challenge
+//   /var/lib/letsencrypt/.well-known/acme-challenge
+
 $ac->loadAccountKey(file_get_contents('cert/account_key.pem'));
 
 $domain_config=array(
-  'wwdev.systems'=>array('challenge'=>'http-01','docroot'=>'../../cloud/vhost/wwdev.systems/')
+  'wwdev.systems'=>array('challenge'=>'http-01','docroot'=>'/var/lib/letsencrypt')
 );
 
 $handler=function($opts){
   $fn=$opts['config']['docroot'].$opts['key'];
-  $result = @mkdir(dirname($fn),0777,true);
-  if ($result) {
-      echo "Directory created successfully.";
-  } else {
-      echo "Failed to create directory.";
-  }
   file_put_contents($fn,$opts['value']);
   echo $fn;
-  /*return function($opts){
+  return function($opts){
     unlink($opts['config']['docroot'].$opts['key']);
-  };*/
+  };
 };
 
 $private_key=$ac->generateRSAKey(2048);
 
 $fullchain=$ac->getCertificateChain($private_key,$domain_config,$handler);
-file_put_contents('fullchain.pem',$fullchain);
-file_put_contents('private_key.pem',$private_key);
+file_put_contents('cert/fullchain.pem',$fullchain);
+file_put_contents('cert/private_key.pem',$private_key);
 ?>
